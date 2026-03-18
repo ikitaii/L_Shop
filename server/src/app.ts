3815ec814e5ea5d29797cdec191ceacf6d269408
Kira <kira95324@gmail.com>
2026-03-19 00:07:21 +0300

    add: cart

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import routes from "./routes";
+import cartRoutes from "./routes/cart.routes"; 
+
 const app = express();
 
-app.use(express.json()); 
-app.get("/", (req, res) => {
-  res.send("API работает");
-});
-app.use("/api", routes);
+app.use(express.json());
+app.use("/cart", cartRoutes); 
+
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+  console.log("Server started");
 });
