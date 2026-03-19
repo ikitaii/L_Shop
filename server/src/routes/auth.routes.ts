87fd193de35eae13dcee343675a878a084a07990
ikitaii <kiselkata339@gmail.com>
2026-03-19 22:50:26 +0300

    add middlewares

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller";
-
+import { isAuth } from "../middlewares/auth.middleware";
+import { addToCart } from "../controllers/cart.controller";
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/cart", isAuth, addToCart);
+router.get("/", isAuth, (req, res) => {
+  res.json({ message: "Это корзина (доступ есть)" });
+});
 
 export default router;
