268e02d9875e1971f4e45812ced76f06d9b4e0b3
kirwwsssq <kira95324@gmail.com>
2026-03-20 00:57:42 +0300

    fix conflict

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,21 @@
 import express from "express";
-import cartRoutes from "./routes/cart.routes";
-import usersRoutes from "./routes/users.routes";
-import productsRoutes from "./routes/products.routes";
+import cookieParser from "cookie-parser";
+
 import usersRoutes from "./routes/users.routes";
 import cartRoutes from "./routes/cart.routes";
 import authRoutes from "./routes/auth.routes";
+import productsRoutes from "./routes/products.routes";
+
 const app = express();
+
 app.use(express.json());
-app.use("/cart", cartRoutes);
+app.use(cookieParser());
+
 app.use("/products", productsRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/cart", cartRoutes);
 app.use("/auth", authRoutes);
+
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
 });
-
